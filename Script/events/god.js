c3f3effe37cd9d0ce3d9781cf0db30811d71215f
qatarlui18-lang <qatarlui18@gmail.com>
2025-12-26 19:18:40 +0530

    Update god.js

--- Script/events/god.js
+++ Script/events/god.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "god",
 	eventType: ["log:unsubscribe", "log:subscribe", "log:thread-name"],
 	version: "1.0.0",
-	credits: "SHAHADAT SAHU",
+	credits: "𝙆𝙞𝙣𝙜 𝘾𝙝𝙖𝙩 𝘽𝙤𝙩",
 	description: "Record bot activity notifications!",
 	envConfig: {
 		enable: true
@@ -13,7 +13,7 @@ module.exports.run = async function({ api, event, Threads }) {
 	const logger = require("../../utils/log");
 	if (!global.configModule[this.config.name].enable) return;
 	
-	let formReport = "=== ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 Notification ===" +
+	let formReport = "=== ─꯭─⃝‌‌𝙆𝙞𝙣𝙜 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭 Notification ===" +
 					"\n\n» Thread ID: " + event.threadID +
 					"\n» Action: {task}" +
 					"\n» Action created by userID: " + event.author +
